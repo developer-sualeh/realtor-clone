fcc8e347d592c073715d51e545dc1dcc5f7233a5
imsualeh <mdsknj@gmail.com>
2023-01-21 15:58:44 +0530

    pages created and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 
-
+import { createBrowserRouter as Router , RouterProvider as Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgetPwd from "./pages/ForgetPwd";
+const router = Router([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/sign-in",
+    element:<SignIn />,
+  },{
+    path: "/sign-up",
+    element: <SignUp />,
+  },{
+    path: "/forget-password",
+    element: <ForgetPwd />,
+  },{
+    path: "/offers",
+    element: <Offers />,
+  },
+]);
 
 function App() {
   return (
-    <div className=''>
-      <h1>hello world</h1>
-    </div>
+    <>
+      
+      <Routes router={router} />
+    </>
   );
 }
 
---
+++ src/pages/ForgetPwd.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ForgetPwd() {
+  return (
+    <div>Forget password </div>
+  )
+}
